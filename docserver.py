41b2b860182c75c93dbe0f2cbaea6b545cea6fa1
Keith Gaughan <k@stereochro.me>
2019-10-07 06:09:40 +0100

    Prevent truncation on upload; release 0.2.1.

--- docserver.py
+++ docserver.py
@@ -40,7 +40,7 @@ from werkzeug.utils import redirect
 from werkzeug.wrappers import Request, Response
 
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 logger = logging.getLogger("docserver")
@@ -320,12 +320,20 @@ class DocServer:
         if len(name) < 2:
             raise BadRequest("Name must be at least two characters long.")
 
+        # HACK: Force the SpooledTemporaryFile to be written to disc. This is
+        # needed otherwise we get the error "'SpooledTemporaryFile' object has
+        # no attribute 'seekable'"
+        content.stream.seekable = lambda: True
+
         try:
             archive = zipfile.ZipFile(content.stream)
             if archive.testzip() is not None:
                 raise BadRequest("Bad Zip file")
         except zipfile.BadZipfile:
             raise BadRequest("Bad Zip file")
+
+        # Reset to the start, or we'll end up with a truncated file.
+        content.stream.seek(0, 0)
 
         catalogue = os.path.join(self.store, name[:2])
         if not os.path.isdir(catalogue):
